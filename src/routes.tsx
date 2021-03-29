cd3eff3cc8fd93d8dff775deda9fc0cee3c66ea3
Levir Cesar <levirlemos@gmail.com>
2021-03-29 19:10:14 -0300

    adicionando rotas, novos componentes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './pages/Landing';
 import TeacherForm from './pages/TeacherForm';
 import TeacherList from './pages/TeacherList';
@@ -10,11 +10,14 @@ import TeacherList from './pages/TeacherList';
 function Routes(){
     return(
         <BrowserRouter>
-            <Route exact  path="/" component={Landing} /> 
-            <Route path="/study" component={TeacherList} />
-            <Route path="/give-classes" component={TeacherForm} />  
+            <Switch>
+                <Route exact  path="/" component={Landing} /> 
+                <Route path="/study" component={TeacherList} />
+                <Route path="/give-classes" component={TeacherForm} />  
+                <Route component={Landing} />  
+            </Switch>
         </BrowserRouter> 
-    );
+    );  
 }
 
 export default Routes;
