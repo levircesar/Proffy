560c5b41087ae96f2444ea89199f4216b6996b41
Levir Cesar <levirlemos@gmail.com>
2021-04-01 17:31:58 -0300

    adicionando pasta serve , pasta web e componente input

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -1,4 +1,4 @@
-import {request, Request , Response } from 'express';
+import { Request , Response } from 'express';
 
 import db from '../database/connection';
 import convertHourToMinutes from '../utils/convertHourToMinutes';
@@ -12,17 +12,34 @@ interface ScheduleItem{
 export default class ClassesController{
   async index(req: Request , res: Response){
     const filters = req.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+    const time = filters.time as string;
+
+
     if(!filters.subject || !filters.week_day || !filters.time ){
       return res.status(400).json({
         error: 'Missing filters to search classes'
       })
     }
 
-    const timeInMinutes = convertHourToMinutes(filters.time as string);
+    const timeInMinutes = convertHourToMinutes(time);
 
-    console.log(timeInMinutes);
-
-    return res.send();
+    const classes = await db('classes')
+      .whereExists(function(){
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ?? ',[Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ?? ',[timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ?? ',[timeInMinutes])        
+        })
+      .where('classes.subject','=',subject)
+      .join('users','classes.user_id','=','users.id')
+      .select('classes.*','users.*');
+    
+    return res.json(classes);
   }
 
   async create(req : Request, res : Response){
