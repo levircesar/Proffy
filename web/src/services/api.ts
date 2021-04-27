3bcb39a68899a63bcd322b6a2af0d647edd05d50
Levir Cesar <levirlemos@gmail.com>
2021-04-26 22:06:18 -0300

    change api and knex file

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:'http://localhost:3333',
+  baseURL:'https://testeserverproffy.herokuapp.com',
+  //'https://testeserverproffy.herokuapp.com'
+  //'http://localhost:3333'
 });
 
 export default api;
